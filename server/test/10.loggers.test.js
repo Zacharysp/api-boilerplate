8d565b2b2cec4801e5168ea70678d509799f678e
Paul Vollmer <paul.vollmer@protonmail.com>
2018-02-08 15:50:03 +0100

    jest-codemods test files

--- server/test/10.loggers.test.js
+++ server/test/10.loggers.test.js
@@ -9,39 +9,39 @@ const app = lib.app;
 describe('The loggers', () => {
   let logger;
 
-  before((done) => {
+  beforeAll((done) => {
     app.boot(done);
   });
 
-  it('should be there', () => {
+  test('should be there', () => {
     lib.should.have.property('logger').which.is.Function();
     lib.should.have.property('loggers').which.is.Object();
   });
 
   describe('The debug', () => {
-    it('should be there', () => {
+    test('should be there', () => {
       lib.loggers.should.have.property('debug').which.is.Function();
     });
 
-    it('can build a logger', () => {
+    test('can build a logger', () => {
       logger = lib.logger('debug');
       logger.should.be.Object();
       logger.should.have.property('info').which.is.Function();
     });
 
-    it('can output', () => {
+    test('can output', () => {
       logger.info(new Error('lorem'));
     });
 
-    it('can output', () => {
+    test('can output', () => {
       logger.info('lorem');
     });
 
-    it('can output', () => {
+    test('can output', () => {
       logger.info({});
     });
 
-    it('can output', () => {
+    test('can output', () => {
       logger.info('lorem', {});
     });
   });
@@ -55,11 +55,11 @@ describe('The loggers', () => {
       }
     });
 
-    it('should be there', () => {
+    test('should be there', () => {
       lib.loggers.should.have.property('ringbuffer').which.is.Function();
     });
 
-    it('can build a logger', () => {
+    test('can build a logger', () => {
       logger = lib.logger('ringbuffer');
       logger.should.be.Object();
       logger.should.have.property('info').which.is.Function();
@@ -67,87 +67,87 @@ describe('The loggers', () => {
       ringbuffer = logger.streams[0].stream;
     });
 
-    it('can output', () => {
+    test('can output', () => {
       logger.info(new Error('lorem'));
       ringbuffer.records.should.be.Array().with.length(1);
     });
 
-    it('can output', () => {
+    test('can output', () => {
       logger.info('lorem');
       ringbuffer.records.should.be.Array().with.length(1);
     });
 
-    it('can output', () => {
+    test('can output', () => {
       logger.info({});
       ringbuffer.records.should.be.Array().with.length(1);
     });
 
-    it('can output', () => {
+    test('can output', () => {
       logger.info('lorem', {});
       ringbuffer.records.should.be.Array().with.length(1);
     });
   });
 
   describe('The syslog', () => {
-    it('should be there', () => {
+    test('should be there', () => {
       lib.loggers.should.have.property('syslog').which.is.Function();
     });
 
-    it('can build a logger', () => {
+    test('can build a logger', () => {
       logger = lib.logger('syslog');
       logger.should.be.Object();
       logger.should.have.property('info').which.is.Function();
     });
 
-    it('can output', () => {
+    test('can output', () => {
       logger.info(new Error('lorem'));
     });
 
-    it('can output', () => {
+    test('can output', () => {
       logger.info('lorem');
     });
 
-    it('can output', () => {
+    test('can output', () => {
       logger.info({});
     });
 
-    it('can output', () => {
+    test('can output', () => {
       logger.info('lorem', {});
     });
   });
 
   describe('The global logger', () => {
-    it('should be there', () => {
+    test('should be there', () => {
       app.should.have.property('logger').which.is.Object();
       app.logger.should.have.property('info').which.is.Function();
     });
 
-    it('can output', () => {
+    test('can output', () => {
       app.logger.info(new Error('lorem'));
     });
 
-    it('can output', () => {
+    test('can output', () => {
       app.logger.info('lorem');
     });
 
-    it('can output', () => {
+    test('can output', () => {
       app.logger.info({});
     });
 
-    it('can output', () => {
+    test('can output', () => {
       app.logger.info('lorem', {});
     });
   });
 
   describe('The middleware', () => {
-    it('can output', () => {
+    test('can output', () => {
       return request(app)
         .get('/api/test')
         .set('Accept', 'application/json')
         .expect(204);
     });
 
-    it('can output', () => {
+    test('can output', () => {
       return request(app)
         .get('/api/test/404')
         .set('Accept', 'application/json')
--- server/test/20.debug.test.js
+++ server/test/20.debug.test.js
@@ -8,56 +8,56 @@ const app = lib.app;
 describe('The debug', () => {
   let debug;
 
-  before((done) => {
+  beforeAll((done) => {
     app.boot(done);
   });
 
-  it('should be there', () => {
+  test('should be there', () => {
     lib.should.have.property('debug').which.is.Function();
   });
 
   describe('An instance with no options', () => {
-    it('can build a debug function', () => {
+    test('can build a debug function', () => {
       debug = lib.debug();
       debug.should.be.Function();
     });
 
-    it('can output', () => {
+    test('can output', () => {
       debug(new Error('lorem'));
     });
 
-    it('can output', () => {
+    test('can output', () => {
       debug('lorem');
     });
 
-    it('can output', () => {
+    test('can output', () => {
       debug({});
     });
 
-    it('can output', () => {
+    test('can output', () => {
       debug('lorem', {});
     });
   });
 
   describe('An instance with a default message', () => {
-    it('can build a debug function', () => {
+    test('can build a debug function', () => {
       debug = lib.debug('default message');
       debug.should.be.Function();
     });
 
-    it('can output', () => {
+    test('can output', () => {
       debug(new Error('lorem'));
     });
 
-    it('can output', () => {
+    test('can output', () => {
       debug('lorem');
     });
 
-    it('can output', () => {
+    test('can output', () => {
       debug({});
     });
 
-    it('can output', () => {
+    test('can output', () => {
       debug('lorem', {});
     });
   });
