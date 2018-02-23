e5f321dc6e817c38052b31244264ec5a5047579b
Paul Vollmer <paul.vollmer@protonmail.com>
2018-02-23 12:39:26 +0100

    Sentry do not handle errors if node env is development

--- server/middleware/sentry.js
+++ server/middleware/sentry.js
@@ -1,14 +1,21 @@
 'use strict';
 
+const app = require('../lib');
 const Raven = require('raven');
 const env = require('env-var');
-const DSN = env.get('SENTRY_DSN', '').asString()
+const DSN = env.get('SENTRY_DSN', '').asString();
 
-if (DSN !== '') {
-  Raven.config(DSN).install();
-  module.exports = function() {
-    return Raven.errorHandler();
-  };
-} else {
-  module.exports = function() {};
+let handler = function() { return function() {}; };
+
+if (DSN !== '' && process.env.NODE_ENV !== 'development' && process.env.NODE_ENV !== 'test') {
+  try {
+    Raven.config(DSN).install();
+    handler = function() {
+      return Raven.errorHandler();
+    };
+  } catch (e) {
+    console.error(`Sentry connection to '${DSN}' failed`);
+  }
 }
+
+module.exports = handler;
