5060b8407ebb767b5d2c4dd3e72fe00815d51252
xavier <xavier@wiredcraft.com>
2019-02-02 22:02:16 +0800

    refactor: healthcheck and test code (#63)
    * refactor: healthcheck and test code
    * chore: update ci with node 10

--- server/boot/health.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-
-module.exports = function(app) {
-  // TODO: more info
-  app.get(app.get('healthApiRoot') || '/health', app.loopback.status());
-};
--- test/e2e/health.test.js
+++ test/e2e/health.test.js
@@ -5,14 +5,15 @@ const request = require('supertest');
 const app = require('../../server/server');
 
 test('health api should return status', () => {
-  request(app)
+  return request(app)
     .get('/health')
     .set('Accept', 'application/json')
     .expect(200)
-    .expect(res => {
-      expect(res).toBeInstanceOf(Object);
-      expect(res.body).toBeInstanceOf(Object);
-      expect(res.body.started).toBeInstanceOf(String);
-      expect(res.body.uptime).toBeInstanceOf(Number);
+    .then(res => {
+      expect(typeof res).toBe('object');
+      expect(typeof res.body).toBe('object');
+      expect(typeof res.body.started).toBe('string');
+      expect(typeof res.body.uptime).toBe('number');
+      expect(res.body.version).toBe('1.0.0');
     });
 });
