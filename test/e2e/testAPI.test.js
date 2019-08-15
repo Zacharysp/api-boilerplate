e8b5f9e7a2c2e16207f3b20e2436c283c43b4f0c
Zacharysp <dongjiezach@gmail.com>
2019-08-15 19:04:47 +0800

    fix: remove unused 204 testcase

--- test/e2e/testAPI.test.js
+++ test/e2e/testAPI.test.js
@@ -4,15 +4,8 @@ const request = require('supertest');
 const app = require('../../server/server');
 
 describe('Test API', () => {
-  it('should return 204 response', () => {
-    request(app)
-      .get('/api/test')
-      .set('Accept', 'application/json')
-      .expect(204);
-  });
-
   it('should return 404 response', () => {
-    request(app)
+    return request(app)
       .get('/api/test/404')
       .set('Accept', 'application/json')
       .expect(404);
